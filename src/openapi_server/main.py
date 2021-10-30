18f04a9eed51d15bde064bb0a35464b5c5f0b4bc
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2021-10-30 23:20:56 +0300

    s3 integration

--- src/openapi_server/main.py
+++ src/openapi_server/main.py
@@ -13,6 +13,7 @@
 from fastapi import FastAPI
 
 from openapi_server.apis.default_api import router as DefaultApiRouter
+from openapi_server.apis.s3_api import router as S3ApiRouter
 
 app = FastAPI(
     title="Swagger Petstore",
@@ -21,3 +22,4 @@ app = FastAPI(
 )
 
 app.include_router(DefaultApiRouter)
+app.include_router(S3ApiRouter)
