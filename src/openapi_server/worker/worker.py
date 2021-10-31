9ffd343cd3497996cf9e156db57ea6dc3e08d99c
Vladimir Kocheryzhkin <vkocherizhkin@gmail.com>
2021-10-31 03:38:44 +0300

    check status

--- src/openapi_server/worker/worker.py
+++ src/openapi_server/worker/worker.py
@@ -4,14 +4,27 @@ import time
 from celery import Celery
 from celery.utils.log import get_task_logger
 
+import urllib.request
+from urllib.parse import urlparse
+
 celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
 celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
 
 logger = get_task_logger(__name__)
 
-@celery.task(name="create_task")
-def create_task(task_type):
-    time.sleep(int(task_type) * 1)
-    logger.info(f'Test task')
+# import glob
+
+
+@celery.task(name="process_file")
+def process_file(url, prefix):
+    # time.sleep(int(task_type) * 1)
+    logger.info(f'Start Processing:')
+    # files = glob.glob("/root/app/store/*")
+    logger.info(f'{prefix} {url}')
+
+    a = urlparse(url)
+    file_name = os.path.basename(a.path)
+    urllib.request.urlretrieve(url, f'/root/app/store/{prefix}_{file_name}')
     return True
+    # return
